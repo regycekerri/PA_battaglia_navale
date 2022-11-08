703936ba7ac172c1a3374867b22c5a6a45a8fd07
MalatestaDavide <86162956+MalatestaDavide@users.noreply.github.com>
2022-11-08 16:13:39 +0100

    Aggiornamento modelli

---
+++ models/orm.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import { Database } from './database';
+
+/**
+ * Instanziazione della connessione verso il database.
+ */
+const connection: Sequelize = Database.getConnection();
+
+/**
+ * Modello che rappresenta la tabella degli utenti nel database (tramite ORM).
+ */
+export const User = connection.define('user', {
+    email: {
+        type: DataTypes.STRING(30),
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING(20),
+        unique: true,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    playing: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    token: {
+        type: DataTypes.DOUBLE(25,2),
+        allowNull: false
+    }
+},
+{
+    modelName: 'user', 
+    timestamps: false,
+    freezeTableName: true
+});
